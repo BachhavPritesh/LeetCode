5c03149c9052cc141ee53f48ebd98cd26e08429a
PRITESH <pritesh.v.bachhav.cg@gmail.com>
2026-04-09 11:13:04 +0530

    Create 14-LongestCommonPrefix

---
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.js
@@ -0,0 +1,16 @@
+// Last updated: 4/9/2026, 11:13:04 AM
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if (!strs.length) return "";
+    let prefix = strs[0];
+    for (let i = 1; i < strs.length; i++) {
+        while (!strs[i].startsWith(prefix)) {
+            prefix = prefix.slice(0, -1);
+            if (prefix === "") return "";
+        }
+    }
+    return prefix;
+};
