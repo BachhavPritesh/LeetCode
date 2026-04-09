e2dd395e7351e7e4e99d91bbc844d564d902f27f
PRITESH <pritesh.v.bachhav.cg@gmail.com>
2026-04-09 11:12:18 +0530

    Create 2752-SumMultiples

---
+++ 2752-SumMultiples/2752-SumMultiples.js
@@ -0,0 +1,16 @@
+// Last updated: 4/9/2026, 11:12:17 AM
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var sumOfMultiples = function(n) {
+    let sum = 0;
+
+    for(let i = 1; i<=n;i++){
+        if(i%3==0||i%5==0||i%7==0){
+            sum+=i;
+        }
+    }
+
+    return sum;
+};
