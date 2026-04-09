6e523a37ce0a047d6a71361e40757beb474df2a2
PRITESH <pritesh.v.bachhav.cg@gmail.com>
2026-04-09 11:12:34 +0530

    Create 151-ReverseWordsInAString

---
+++ 151-ReverseWordsInAString/151-ReverseWordsInAString.js
@@ -0,0 +1,10 @@
+// Last updated: 4/9/2026, 11:12:33 AM
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+  return s.trim().split(/\s+/).reverse().join(' ');
+
+    
+};
