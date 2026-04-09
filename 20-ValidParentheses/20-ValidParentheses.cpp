d803950c5f34282f9026ec8ab7c72e44eaa9f570
PRITESH <pritesh.v.bachhav.cg@gmail.com>
2026-04-09 11:12:57 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.cpp
@@ -0,0 +1,26 @@
+// Last updated: 4/9/2026, 11:12:57 AM
+class Solution {
+public:
+    bool isValid(string s) {
+           stack<char> st;
+
+    for (char c : s) {
+        if (c == '(' || c == '{' || c == '[') {
+            st.push(c);
+        } 
+        else {
+            if (st.empty()) return false;
+            char top = st.top();
+            st.pop();
+            if ((c == ')' && top != '(') ||
+                (c == '}' && top != '{') ||
+                (c == ']' && top != '[')) {
+                return false;
+            }
+        }
+    }
+
+  
+    return st.empty();
+    }
+};
