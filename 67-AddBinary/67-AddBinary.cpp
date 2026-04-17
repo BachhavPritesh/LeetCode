c4219e4a48fc60ac3eb3ccdfb27d3a1e6ff3ffa7
PRITESH <pritesh.v.bachhav.cg@gmail.com>
2026-04-17 19:48:33 +0530

    Create 67-AddBinary

---
+++ 67-AddBinary/67-AddBinary.cpp
@@ -0,0 +1,17 @@
+// Last updated: 4/17/2026, 7:48:33 PM
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result = "";
+        int i = a.size() - 1, j = b.size() - 1, carry = 0;
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
+            result.push_back((sum % 2) + '0'); 
+            carry = sum / 2;                  
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
