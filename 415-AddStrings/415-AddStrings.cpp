d0522bdde5bbd75e44b6739576d24727cc4b6150
PRITESH <pritesh.v.bachhav.cg@gmail.com>
2026-05-12 14:40:50 +0530

    Create 415-AddStrings

---
+++ 415-AddStrings/415-AddStrings.cpp
@@ -0,0 +1,24 @@
+// Last updated: 5/12/2026, 2:40:49 PM
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int i =num1.size()-1;
+        int j =num2.size()-1;
+
+        string res = "";
+
+        int carry = 0;
+        while(i>=0||j>=0||carry){
+            int d1 = (i>=0)?num1[i]-'0':0;
+            int d2 = (j>=0)?num2[j]-'0':0;
+
+            int sum = d1+d2+carry;
+            carry = sum/10;
+            res.push_back((sum%10)+'0');
+            i--;
+            j--;
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
