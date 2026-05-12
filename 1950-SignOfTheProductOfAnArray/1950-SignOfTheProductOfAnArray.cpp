1dc659f896c6860c4733eb2527839e5a0f94e386
PRITESH <pritesh.v.bachhav.cg@gmail.com>
2026-05-12 14:39:47 +0530

    Create 1950-SignOfTheProductOfAnArray

---
+++ 1950-SignOfTheProductOfAnArray/1950-SignOfTheProductOfAnArray.cpp
@@ -0,0 +1,17 @@
+// Last updated: 5/12/2026, 2:39:46 PM
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int sign = 1;
+
+        for(int num:nums){
+            if(num==0){
+                return 0;
+            }
+            if(num<0){
+                sign=-sign;
+            }
+        }
+        return sign;
+    }
+};
