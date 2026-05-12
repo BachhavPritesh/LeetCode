02d2ed8d4a6420c06f932a09a0a8327401149bbd
PRITESH <pritesh.v.bachhav.cg@gmail.com>
2026-05-12 14:40:15 +0530

    Create 1468-CheckIfNAndItsDoubleExist

---
+++ 1468-CheckIfNAndItsDoubleExist/1468-CheckIfNAndItsDoubleExist.cpp
@@ -0,0 +1,18 @@
+// Last updated: 5/12/2026, 2:40:14 PM
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> s;
+        for(int num:arr){
+            if(s.count(num*2)){
+                return true;
+            }
+
+            if(num%2==0 && s.count(num/2)){
+                return true;
+            }
+            s.insert(num);
+        }
+        return false;
+    }
+};
