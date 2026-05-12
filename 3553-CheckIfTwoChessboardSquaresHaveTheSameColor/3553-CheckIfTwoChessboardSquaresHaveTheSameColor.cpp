0b69fa5683980698faf0b59bc4dea8c073d05d6a
PRITESH <pritesh.v.bachhav.cg@gmail.com>
2026-05-12 14:39:17 +0530

    Create 3553-CheckIfTwoChessboardSquaresHaveTheSameColor

---
+++ 3553-CheckIfTwoChessboardSquaresHaveTheSameColor/3553-CheckIfTwoChessboardSquaresHaveTheSameColor.cpp
@@ -0,0 +1,13 @@
+// Last updated: 5/12/2026, 2:39:16 PM
+class Solution {
+public:
+    bool checkTwoChessboards(string coordinate1, string coordinate2) {
+        int c1 = coordinate1[0]-'a';
+        int r1 = coordinate1[1]-'1';
+
+        int c2 = coordinate2[0]-'a';
+        int r2 = coordinate2[1]-'1';
+
+        return ((c1+r1)%2)==((c2+r2)%2);
+    }
+};
