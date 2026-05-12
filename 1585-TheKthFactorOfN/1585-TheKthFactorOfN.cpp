d46f6a126d9073314a7c5f9a74b6992f3504d268
PRITESH <pritesh.v.bachhav.cg@gmail.com>
2026-05-12 14:40:07 +0530

    Create 1585-TheKthFactorOfN

---
+++ 1585-TheKthFactorOfN/1585-TheKthFactorOfN.cpp
@@ -0,0 +1,22 @@
+// Last updated: 5/12/2026, 2:40:06 PM
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        vector<int> res;
+
+        for(int i = 1;i<=n;i++){
+            if(n%i==0){
+                res.push_back(i);
+            }
+        }
+
+        if(res.size()<k){
+             return -1;
+        }
+        else{
+            return res[k-1];
+        }
+
+       
+    }
+};
