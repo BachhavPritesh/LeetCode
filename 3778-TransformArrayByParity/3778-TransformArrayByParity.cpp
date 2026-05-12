7651b7bbf6e2c0bbf429d6017f0f7458e73b13c6
PRITESH <pritesh.v.bachhav.cg@gmail.com>
2026-05-12 14:39:08 +0530

    Create 3778-TransformArrayByParity

---
+++ 3778-TransformArrayByParity/3778-TransformArrayByParity.cpp
@@ -0,0 +1,17 @@
+// Last updated: 5/12/2026, 2:39:07 PM
+class Solution {
+public:
+    vector<int> transformArray(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0){
+                nums[i]=0;
+            }
+            else{
+                nums[i]=1;
+            }
+        }
+        sort(nums.begin(),nums.end());
+
+        return nums;
+    }
+};
