5d3bf6b6ad11a6e6a5b2ad83ace308ee350d6d9f
PRITESH <pritesh.v.bachhav.cg@gmail.com>
2026-05-12 14:39:32 +0530

    Create 2298-CountIntegersWithEvenDigitSum

---
+++ 2298-CountIntegersWithEvenDigitSum/2298-CountIntegersWithEvenDigitSum.cpp
@@ -0,0 +1,22 @@
+// Last updated: 5/12/2026, 2:39:30 PM
+class Solution {
+public:
+    int digitSum(int n){
+        int sum = 0;
+        while(n > 0){
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    int countEven(int num) {
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            if (digitSum(i) % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
