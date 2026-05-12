b976b2d47f09acabc765a89fbc682695d4218736
PRITESH <pritesh.v.bachhav.cg@gmail.com>
2026-05-12 14:39:14 +0530

    Create 3582-FindIndicesOfStableMountains

---
+++ 3582-FindIndicesOfStableMountains/3582-FindIndicesOfStableMountains.cpp
@@ -0,0 +1,13 @@
+// Last updated: 5/12/2026, 2:39:12 PM
+class Solution {
+public:
+    vector<int> stableMountains(vector<int>& height, int threshold) {
+        vector<int> res;
+        for(int i=1;i<height.size();i++){
+            if(height[i-1]>threshold){
+                res.push_back(i);
+            }
+        }
+        return res;
+    }
+};
