0cc45fa08c51486cad812f42b3b72fd17104df9d
PRITESH <pritesh.v.bachhav.cg@gmail.com>
2026-05-16 19:23:53 +0530

    Create 2536-DetermineIfTwoEventsHaveConflict

---
+++ 2536-DetermineIfTwoEventsHaveConflict/2536-DetermineIfTwoEventsHaveConflict.cpp
@@ -0,0 +1,10 @@
+// Last updated: 5/16/2026, 7:23:49 PM
+class Solution {
+public:
+    bool haveConflict(vector<string>& event1, vector<string>& event2) {
+        if(event1[0]>event2[1]||event2[0]>event1[1]){
+            return false;
+        }
+        return true;
+    }
+};
