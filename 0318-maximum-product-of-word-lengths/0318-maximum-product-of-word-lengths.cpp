7fa2283e6374717d3d003d6f3fb400344aa0769d
PRITESH <pritesh.v.bachhav.cg@gmail.com>
2026-08-13 16:04:55 +0530

    Time: 43 ms (24.16%), Space: 29.7 MB (19.75%) - LeetHub

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.cpp
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
     int maxProduct(vector<string>& words) {
+        int product = 0;
         int n = words.size();
-        vector<int> mask(n, 0);
+
+        vector<int> mask(26 * n, 0);
 
         for (int i = 0; i < n; i++) {
             for (char c : words[i]) {
-                mask[i] |= (1 << (c - 'a'));
+                mask[(26 * i) + (c - 'a')]++;
             }
         }
 
-        int ans = 0;
-
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                if ((mask[i] & mask[j]) == 0) {
-                    ans = max(ans, (int)words[i].size() * (int)words[j].size());
+
+                bool common = false;
+
+                for (int k = 0; k < 26; k++) {
+                    if (mask[(26 * i) + k] &&
+                        mask[(26 * j) + k]) {
+
+                        common = true;
+                        break;
+                    }
+                }
+
+                if (!common) {
+                    int curr =
+                        words[i].size() *
+                        words[j].size();
+
+                    product = max(product, curr);
                 }
             }
         }
 
-        return ans;
+        return product;
     }
 };
